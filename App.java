f96550cd9084f68bd8644940ac976b0863557378
Leonardo014 <leonardo.oliveira6531@gmail.com>
2023-09-06 12:36:15 -0300

    "Refatorando" o cód

--- App.java
+++ App.java
@@ -1,29 +1,11 @@
-package CRC;
+//package CRC;
 
-public class App {
+public class Main {
+    public static void main(String[] args) {
+     Paciente paciente = new Paciente(30.0, 1.70);
+     paciente.calculaIMC();
+     System.out.println("Seu peso eh: " + paciente.getPesoKG() + "\n" + "Sua altura eh: " + paciente.getAlturaMt());
+     System.out.println("O valor do seu IMC eh: " + paciente.getImc());
 
-	public static void main(String[] args) {
-		Paciente paciente = new Paciente(52.0, 1.56, 0.0);
-		double imc = paciente.getImc();
-		
-		if (imc < 16.0) {
-			System.out.println("Baixo peso muito grave");
-		} else if (imc >= 16.0 && imc < 17.0) {
-			System.out.println("Baixo peso grave");
-		} else if (imc >= 17.0 && imc < 18.5) {
-			System.out.println("Baixo peso");
-		} else if (imc >= 18.5 && imc < 25.0) {
-			System.out.println("Peso normal");
-		} else if (imc >= 25.0 && imc < 30.0) {
-			System.out.println("Sobrepeso");
-		} else if (imc >= 30.0 && imc < 35.0) {
-			System.out.println("Obesidade grau I");
-		} else if (imc >= 35.0 && imc < 40.0) {
-			System.out.println("Obesidade grau II");
-		} else {
-			System.out.println("Obesidade grau III (obesidade morbido"); 
-		}
-	
-	}
-
+    }
 }
--- Paciente.java
+++ Paciente.java
@@ -1,30 +1,46 @@
-package CRC;
-
 public class Paciente {
-	double pesoKG;
-	double alturaMt;
-	double imc;
-	
-	public Paciente(double pesoKG, double alturaMt, double imc) {
-		this.pesoKG = pesoKG;
-		this.alturaMt = alturaMt;
-		this.imc = imc;
-	}
-	
-	public double getPesoKG() {
-		return pesoKG;
-	}
-	public double getAlturaMt() {
-		return alturaMt;
-	}
-	public double getImc() {
-		imc = pesoKG / (alturaMt * alturaMt); 
-		return imc;
-	}
-	
-	/*public double CalcularImc() {
-		imc = pesoKG / (alturaMt * alturaMt); 
-	
-		return imc;
-	}*/
+    double alturaMt;
+    double pesoKG;
+    double imc;
+
+    public Paciente(double pesoKG, double alturaMt){
+        this.alturaMt = alturaMt;
+        this.pesoKG = pesoKG;
+    }
+
+    public double getAlturaMt() {
+        return alturaMt;
+    }
+
+    public double getPesoKG() {
+        return pesoKG;
+    }
+
+    public double getImc() {
+        imc = pesoKG / (alturaMt * alturaMt);
+        return imc;
+    }
+
+    public double calculaIMC(){
+
+        if (imc < 16.0) {
+            System.out.println("Baixo peso muito grave");
+        } else if (imc >= 16.0 && imc < 17.0) {
+            System.out.println("Baixo peso grave");
+        } else if (imc >= 17.0 && imc < 18.5) {
+            System.out.println("Baixo peso");
+        } else if (imc >= 18.5 && imc < 25.0) {
+            System.out.println("Peso normal");
+        } else if (imc >= 25.0 && imc < 30.0) {
+            System.out.println("Sobrepeso");
+        } else if (imc >= 30.0 && imc < 35.0) {
+            System.out.println("Obesidade grau I");
+        } else if (imc >= 35.0 && imc < 40.0) {
+            System.out.println("Obesidade grau II");
+        } else {
+            System.out.println("Obesidade grau III (obesidade morbido");
+        }
+        return imc;
+    }
+
 }
